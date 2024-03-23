65c339cdafff33f89f22ed5d4adf82fce1c18b53
Rohan Kumar <96616262+rk4rohankumar@users.noreply.github.com>
2024-03-23 21:21:06 +0530

    hexleap

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,17 +6,15 @@ const Navbar = () => {
       <nav className="bg-white border-gray-200 dark:bg-gray-900 text-black dark:text-white">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
-            href="https://flowbite.com/"
+            href="https://hexleap.com/"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <img
-              src="https://flowbite.com/docs/images/logo.svg"
+              src="https://www.hexleap.com/_next/static/media/logo-full.190fb9ab.svg"
               className="h-8"
-              alt="Flowbite Logo"
+              alt="Hexleap Logo"
             />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Hexleap
-            </span>
+            
           </a>
           <button
             data-collapse-toggle="navbar-default"
